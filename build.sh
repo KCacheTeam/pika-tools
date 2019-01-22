66640a7fc7c88c7a5da6080b45487b62aa244380
AlexStocks <alexstocks@foxmail.com>
2019-01-22 21:08:40 +0800

    Upd: add compression libs

--- build.sh
+++ build.sh
@@ -9,6 +9,22 @@
 # FILE    : build.sh
 # ******************************************************
 
+# install gflags
+git clone https://github.com/gflags/gflags.git
+cd gflags
+git checkout v2.0
+./configure && make && sudo make install
+cd ..
+
+# install compression lib
+sudo yum install -y snappy snappy-devel
+sudo yum install -y zlib zlib-devel
+sudo yum install -y bzip2 bzip2-devel
+sudo yum install -y lz4-devel
+
+# download third libs
 # pls wait for about 30 minutes
 git submodule update --init --recursive --force
+
+# compile pika-port
 cd pika_port && make
